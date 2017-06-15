9ea0e89406021bb95b4137203e2e15944bfb91c7
Sean Thomas <Sean.M.Thomas.0@gmail.com>
2017-06-14 22:29:10 -0700

    Add solutions from first attempt at ex5

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -1,19 +1,27 @@
 function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
-%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
+%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear
 %regression with multiple variables
-%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
-%   cost of using theta as the parameter for linear regression to fit the 
+%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the
+%   cost of using theta as the parameter for linear regression to fit the
 %   data points in X and y. Returns the cost in J and the gradient in grad
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+% You need to return the following variables correctly
+h = X*theta;
+theta_reg = theta;
+theta_reg(1) = 0;
+z = h - y;
+J_reg = theta_reg' * theta_reg * lambda;
+J_cost = z' * z;
+J = (J_cost + J_reg) / (2 * m);
+grad_reg = lambda * theta_reg;
+grad_cost = X' * z;
+grad = (grad_cost + grad_reg) / m;
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost and gradient of regularized linear 
+% Instructions: Compute the cost and gradient of regularized linear
 %               regression for a particular choice of theta.
 %
 %               You should set J to the cost and grad to the gradient.
--- machine-learning-ex5/ex5/polyFeatures.m
+++ machine-learning-ex5/ex5/polyFeatures.m
@@ -7,13 +7,15 @@ function [X_poly] = polyFeatures(X, p)
 
 
 % You need to return the following variables correctly.
-X_poly = zeros(numel(X), p);
-
+X_poly = [];
+for i = 1:p
+    X_poly = [X_poly X .^ i];
+end
 % ====================== YOUR CODE HERE ======================
-% Instructions: Given a vector X, return a matrix X_poly where the p-th 
+% Instructions: Given a vector X, return a matrix X_poly where the p-th
 %               column of X contains the values of X to the p-th power.
 %
-% 
+%
 
 
 
