cf60e8b88d32b77d6f3b36b677ae5a0927402f5f
Sean Thomas <Sean.M.Thomas.0@gmail.com>
2017-06-17 23:20:01 -0700

    Add work from first attempt

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,20 +15,30 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
-
-
-
-
-
-
-
+C_list = [0.01 0.03 0.1 0.3 1 3 10 30];
+sig_list = [0.01 0.03 0.1 0.3 1 3 10 30];
+n = length(C_list);
+m = length(sig_list);
+val_err = zeros(n, m);
+for i=1:n
+    c = C_list(i);
+    for j=1:m
+        sig = sig_list(j);
+        model = svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, sig));
+        predictions = svmPredict(model, Xval);
+        val_err(i,j) = mean(double(predictions ~= yval));
+    end
+end
+[rows, cols] = find(val_err == min(min(val_err)));
+C = C_list(rows(1));
+sigma = sig_list(cols(1));
 % =========================================================================
 
 end
--- machine-learning-ex6/ex6/gaussianKernel.m
+++ machine-learning-ex6/ex6/gaussianKernel.m
@@ -7,8 +7,8 @@ function sim = gaussianKernel(x1, x2, sigma)
 x1 = x1(:); x2 = x2(:);
 
 % You need to return the following variables correctly.
-sim = 0;
-
+dist = x1 - x2;
+sim = exp(-(dist' * dist)/(2 * sigma ^ 2));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the similarity between x1
 %               and x2 computed using a Gaussian kernel with bandwidth
@@ -22,5 +22,5 @@ sim = 0;
 
 
 % =============================================================
-    
+
 end
